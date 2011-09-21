d24db1883d9eea69848d991095527996bb3d33e2
Jonathan Hinkle <hynkle@gmail.com>
2011-09-21 13:51:47 -0400

    Remove date and time extensions (conflicts with ActiveSupport)

--- lib/ruby-units.rb
+++ lib/ruby-units.rb
@@ -2,8 +2,6 @@ $LOAD_PATH << File.dirname(__FILE__)
 require "ruby_units/version"
 require "ruby_units/cache"
 require 'ruby_units/array'
-require 'ruby_units/date'
-require 'ruby_units/time'
 require 'ruby_units/math'
 require 'ruby_units/numeric'
 require 'ruby_units/object'
